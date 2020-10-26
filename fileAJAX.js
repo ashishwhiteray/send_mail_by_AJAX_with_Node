76cd93b1a513d9a259022ea82dcb08f7653fea72
Ashish Saxena <54023491+ashishwhiteray@users.noreply.github.com>
2020-10-27 02:12:21 +0530

    Update fileAJAX.js

--- fileAJAX.js
+++ fileAJAX.js
@@ -1,1 +1,19 @@
 
+    var name= "Ashish";           //-----------This is simple data  variable-----------------
+    var email= "ashish@gmail.com";            //-----------This is simple data in variable-----------------
+  
+    $.ajax({
+        method: 'POST',
+        type: 'json',
+        url: "http://localhost:3000",
+        data: {name: name, email: email},    //-----------We have created an object of data-----------------
+        success: function( data ){      //-----------This is data to be send-----------------
+            if(data){
+                console.log("This is response data we are getting from node at backend "+data);         //-----------This is the data we are receiving from response of server-----------------
+            }
+        },
+        error: function (request, status, error) {
+             console.log("This is the error "+error);
+        }
+		});
+
