e6d15aa64354aa40552ff2099fb57d8ddd801744
Ashish Saxena <54023491+ashishwhiteray@users.noreply.github.com>
2020-10-27 02:04:03 +0530

    Update apiNode.js

--- apiNode.js
+++ apiNode.js
@@ -1,1 +1,21 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require ('cors');
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use(cors());
+
+//--------------Creating POST API--------------
+
+app.post('/', async function(req,res){
+
+    console.log("This is the JSON data, getting from AJAX on frontend ", req.body);
+   
+})
+app.listen('3000', function(){
+    console.log("Server is running on port no. 3000");
+});
